711079cf46429082fec6329e042dfe478dcebc15
Logan Sroufe <lsroufe@gmail.com>
2016-08-31 10:15:18 -0700

    updated html css and js

--- js/main.js
+++ js/main.js
@@ -54,6 +54,8 @@ var swFilms = new Bloodhound({
 // Function to send search term request to 500px API and return 20 images
 var getPix = function(term) {
 
+	term = 'star+wars ' + term;
+
 	var consumer_key = 'r41HCJJ4F3iIJZeFeacW0RCHFXMCBe5TdoT2cclI';
 	var url = 'https://api.500px.com/v1/photos/search?term=' + term;
 
@@ -61,13 +63,14 @@ var getPix = function(term) {
 		'url': url,
 		'data': {
 			'consumer_key' : consumer_key,
-			'image_size' : 1080,
+			'image_size' : 600,
 			'sort' : 'rating',
 			'exclude' : 'nude'
 		}
 	})
 		.done(function(data) {
 	    console.log( "Sample of data:", data );
+
 	    $.each(data.photos, function(index, item) {
 				var swPhoto = showPix(item);
 				$('#results').append(swPhoto);
@@ -77,36 +80,41 @@ var getPix = function(term) {
 
 // Function to display the images on screen
  var showPix = function(photo) {
- 	var image = "<img src='" + photo.image_url + "'>";
+ 	
+ 	var image = "<div class='col-xs-12 col-md-3'><div class='thumbnail'><img class='img-responsive' src='" + photo.image_url + "'></div></div>";
  	return image;
  };
 
 $(document).ready(function() {
-	$('#multiple-datasets .typeahead').typeahead({
-	  highlight: true
-	},
-	{
-	  name: 'sw-names',
-	  display: 'name',
-	  source: swPeople,
-	  templates: {
-	    header: '<h3 class="star-wars">Star Wars Characters</h3>'
-	  }
-	},
-	{
-	  name: 'sw-films',
-	  display: 'title',
-	  source: swFilms,
-	  templates: {
-	    header: '<h3 class="star-wars">Star Wars Films</h3>'
-	  }
+
+	// initiate the typeahead input field (auto completion)
+	$('#multiple-datasets .typeahead').typeahead(
+		{
+		  highlight: true
+		},
+		{
+		  name: 'sw-names',
+		  display: 'name',
+		  source: swPeople,
+		  templates: {
+		    header: '<h3 class="star-wars">Star Wars Characters</h3>'
+		  }
+		},
+		{
+		  name: 'sw-films',
+		  display: 'title',
+		  source: swFilms,
+		  templates: {
+		    header: '<h3 class="star-wars">Star Wars Films</h3>'
+		  }
 	});
 
-	$('#results').html('');
-	
+	// when a search result is selected
 	$('.typeahead').bind('typeahead:select', function(ev, suggestion) {
   	console.log('Selection: ' + suggestion.name);
+  	$('#results').html('');
   	getPix(suggestion.name);
 	});
 
+	getPix('Star Wars Luke Skywalker');
 });
